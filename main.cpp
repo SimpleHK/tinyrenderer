6881007ac5356d03e35cfbb7253d9ca8cb10fe75
fred.ji <123@qq.com>
2023-05-21 10:39:27 +0800

    用直线方程基于x坐标采样。

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ const TGAColor white = {
     .bgra = {255, 255, 255, 255},
 };
 const TGAColor red = {
-    .bgra = {255, 0, 0, 255},
+    .bgra = {0, 0, 255, 255},
 };
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
@@ -16,11 +16,31 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
   }
 }
 
+/* 用直线方程以x坐标为基准对线段上的点进行采样
+ * (x - x0) / (x1 - x0) = t = (y - y0) / (y1 - y0)
+ * 缺点：因为以x坐标采样，当(y1 - y0) > (x - x0)时采样次数 < 线段上实际的像素点数量，导致线段不连续
+ */
+void line2(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
+{
+  for (int x = x0; x <= x1; x++) {
+    float t = (x - x0) / static_cast<float>(x1 - x0); // 避免使用整数除法，导致 t = 0
+    int y = static_cast<int>(y0 * (1.0f - t) + y1 * t);
+    image.set(x, y, color);
+  }
+}
+
 int main(int argc, char **argv) {
   TGAImage image(100, 100, TGAImage::RGB);
-  line(13, 20, 80, 40, image, white);
-  image.flip_vertically(); // i want to have the origin at the left bottom
-                           // corner of the image
+  line2(0,    0,    100,  100, image, white);   // y = x 采样次数 >= 线段上实际的像素点数量
+  line2(0,    0,    50,   100, image, red);     // y = 2x 采样次数 < 线段上实际的像素点数量
+  line2(100,  100,  0,    0,   image, white);   // x0(y0)大于x1(y1)的情况
+  
+  // line2(13, 20, 80, 40, image, white); 
+  // line2(20, 13, 40, 80, image, red);
+  // line2(80, 40, 13, 20, image, red);
+
+  // image.flip_vertically(); // i want to have the origin at the left bottom, 新代码原点默认在左下角
+                              // corner of the image
   image.write_tga_file("output.tga");
   return 0;
 }
